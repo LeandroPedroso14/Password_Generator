2a1c7ed261d77d0deebe72ec8d5957a0363b8cb1
Leandro Pedroso <lpedroso64@outlook.com>
2023-01-23 20:29:23 -0300

    Code indentation and parentheses

--- password.py
+++ password.py
@@ -1,34 +1,35 @@
 import random
 import PySimpleGUI as sg
-import os 
+import os
+
 
 class PassGen:
-   def __init__(self):
-      #Layout
-      sg.theme('Black')
-      layout = [
-          [sg.Text('Login',size=(10,1)),
-           sg.Input(key='site', size=(20,1))],
-          [sg.Text('E-mail',size=(10,1)),
-           sg.Input(key='usuario', size=(20,1))],
-          [sg.Text('Quantidade de caracteres'), sg.Combo(values=list(
-              range(30)),key='total_chars', defoult_value=1, size=(3,1))]
-          [sg.Output(size=(32, 5))],
-          [sg.Button('Gerar senha')]
-           
 
-      ]
-      #Declarar Janela
-      self.janela = sg.Window('Password Generator',layout)
+    def __init__(self):
+       # Layout
+        sg.theme('Black')
+        layout = [
+            [sg.Text('Plataforma', size=(10, 1)),
+             sg.Input(key='Sistema', size=(20, 1))],
+            [sg.Text('E-mail', size=(10, 1)),
+             sg.Input(key='usuario', size=(20, 1))],
+            [sg.Text('Quantidade de caracteres'),
+             sg.Combo(values=list(range(30)), key='total_chars', default_value=1, size=(3, 1))],
+            [sg.Output(size=(32, 5))],
+            [sg.Button('Gerar senha')],
+        ]
+        # Declarar Janela
+        self.janela = sg.Window('Password Generator', layout)
 
-   def Iniciar(self):
-       while True:
-           evento,valores = self.janela.read()
-           if evento == sg.WINDOW_CLOSED:
-               break
+    def Iniciar(self):
+        while True:
+            evento, valores = self.janela.read()
+            if evento == sg.WINDOW_CLOSED:
+                break
 
-   def salvar_senha(self):
-      pass
+    def salvar_senha(self):
+        pass
+
 
 gen = PassGen()
 gen.Iniciar()
