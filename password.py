123d4f2181944cec32932c2537d80ee637aa16df
Leandro Pedroso <lpedroso64@outlook.com>
2023-01-23 14:23:54 -0300

    defining text

--- password.py
+++ password.py
@@ -6,9 +6,9 @@ class PassGen:
    def __init__(self):
       sg.theme('Black')
       layout = [
-          [sg]
+          [sg.Text('Login')]
       ]
-      
+
    def Iniciar(self):
       pass
    def salvar_senha(self):
